2476901644baede0da764ccc3c35e557e5762655
chowdapon2 <chowdapon@gmail.com>
2018-08-15 23:32:52 +0800

    LeetCode][Easy][485] Max Consecutive Ones

---
+++ 485.max-consecutive-ones.java
@@ -0,0 +1,48 @@
+/*
+ * [485] Max Consecutive Ones
+ *
+ * https://leetcode.com/problems/max-consecutive-ones/description/
+ *
+ * algorithms
+ * Easy (53.74%)
+ * Total Accepted:    97.1K
+ * Total Submissions: 180.6K
+ * Testcase Example:  '[1,0,1,1,0,1]'
+ *
+ * Given a binary array, find the maximum number of consecutive 1s in this
+ * array.
+ * 
+ * Example 1:
+ * 
+ * Input: [1,1,0,1,1,1]
+ * Output: 3
+ * Explanation: The first two digits or the last three digits are consecutive
+ * 1s.
+ * ⁠   The maximum number of consecutive 1s is 3.
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The input array will only contain 0 and 1.
+ * The length of input array is a positive integer and will not exceed 10,000
+ * 
+ * 
+ */
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+        	count++;
+        	result = Math.max(count, result);
+            }else{
+                count = 0;
+            }
+        }
+        
+        return result;
+    }
+}
