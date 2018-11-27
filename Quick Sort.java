efdbef6d7ff4010d6344b201e0f9e5ded4140363
Kevin_Chiou <Kevin_Chiou@asus.com>
2018-11-27 09:50:58 +0800

    Quick sort implement

---
+++ Quick Sort.java
@@ -0,0 +1,55 @@
+    /* This function takes last element as pivot, 
+       places the pivot element at its correct 
+       position in sorted array, and places all 
+       smaller (smaller than pivot) to left of 
+       pivot and all greater elements to right 
+       of pivot */
+    int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1); // index of smaller element ,i會從-1開始
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than or 
+            // equal to pivot 
+            if (arr[j] <= pivot) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+    /* The main function that implements QuickSort() 
+      arr[] --> Array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    void sort(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            int pi_index = partition(arr, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            
+            // pivot 左邊
+            sort(arr, low, pi_index-1); 
+            // pivot 左邊
+            sort(arr, pi_index+1, high); 
+        } 
+    } 
