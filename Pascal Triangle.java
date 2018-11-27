68ef6f1145c02d74df5cd5d8a938a5dcc7f8ad4b
Kevin_Chiou <Kevin_Chiou@asus.com>
2018-11-27 09:53:51 +0800

    update [118]pascal triangle

--- Pascal Triangle.java
+++ Pascal Triangle.java
@@ -16,34 +16,45 @@ Return
 
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-                //要用ArrayList建造  不然會GG
-        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        //要用ArrayList建造  不然會GG
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        // A[x][y] = 1 in start and end
+        // A[x][y] = A[x-1][y-1]+ A[x-1][y]
         
-        if(numRows == 0){
-            return triangle;
-        }
+        if(numRows ==0) return result;
         
         //can't use new ArrayList<Integer>(1); 
         //it may cause error
         List<Integer> sub = new ArrayList<Integer>();
         sub.add(1);
-        triangle.add(sub);
-        for(int i = 1; i < numRows; i++){
-            List<Integer> row = new ArrayList<Integer>();
-            List<Integer> prevRow = triangle.get(i-1);//拿上一個row, 整行拿
+        result.add(sub);
+        
+        // every row's element's number = it's row number
+        for(int i =1; i< numRows; i++){
+            // take upper row
+            // to get upper row use .get() function!!
+            List<Integer> upper_row = result.get(i-1);
             
-            //beginning
-            row.add(1);
+            List<Integer> cur_row = new ArrayList<Integer>();
             
-            for(int j = 1; j<i ; j++){
-                row.add(prevRow.get(j-1)+prevRow.get(j));
+            // add one at first
+            cur_row.add(1);
+            
+            // every row's element's number = it's row number
+            //i<j also can use j<upper_row.size()-1 but slower
+            for(int j =1 ;j<i; j++ ){
+                cur_row.add(upper_row.get(j-1)+upper_row.get(j));
             }
-            //end
-            row.add(1);
             
-            triangle.add(row);
+            // add one at the end
+            cur_row.add(1);
+            
+            result.add(cur_row);
+            
         }
         
-        return triangle;
+        return result;
+        
+        
     }
 }
