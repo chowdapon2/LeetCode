3ac33e0fe5445637660d584162e6af557004d7f2
Kevin_Chiou <Kevin_Chiou@asus.com>
2019-03-11 15:38:12 +0800

    Create testRun.java

---
+++ testRun.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.IOException;
+
+
+class testRun {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("test1" );
+    }
+}
